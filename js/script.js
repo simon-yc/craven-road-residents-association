bdc97fda59d819182169caed82e83103badf5cb1
syc <83619071+simon-yc@users.noreply.github.com>
2024-04-27 23:21:54 -0400

    responsive navbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector('nav-bar');
+const menuLinks = document.querySelector('#menu-links');
+
+function toggleMenu() {
+    menuLinks.classList.toggle("show-menu");
+}
