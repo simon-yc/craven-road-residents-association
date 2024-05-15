0e66c81227d01b9e79abfb18dab7f7cab224878a
syc <83619071+simon-yc@users.noreply.github.com>
2024-05-14 22:02:35 -0400

    WEIRD WORKAROUND FOR DEPLOYMENT

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  fetch('../pages/navbar.html')
+  fetch('../navbar.html')
     .then(response => response.text())
     .then(html => {
       document.getElementById('navbar-placeholder').innerHTML = html;
