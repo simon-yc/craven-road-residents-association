5f3d70fdc6c35a0be55c86a014db5be4d7081978
syc <83619071+simon-yc@users.noreply.github.com>
2024-06-03 04:00:10 -0400

    fix slider

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const slides = document.querySelector('.slides');
-    const allSlides = document.querySelectorAll('.slide');
-    let index = 0;
+  const sliders = document.querySelectorAll('.slider');
   
-    function showSlide(n) {
-      if (n >= allSlides.length) index = 0;
-      if (n < 0) index = allSlides.length - 1;
-      slides.style.transform = `translateX(-${index * 100}%)`;
-    }
-  
-    document.querySelector('.prev').addEventListener('click', () => {
-      showSlide(--index);
-    });
-  
-    document.querySelector('.next').addEventListener('click', () => {
-      showSlide(++index);
-    });
+  sliders.forEach(slider => {
+      const slides = slider.querySelector('.slides');
+      const allSlides = slider.querySelectorAll('.photo-item');
+      let index = 0;
+
+      function showSlide(n) {
+          if (n >= allSlides.length) index = 0;
+          if (n < 0) index = allSlides.length - 1;
+          slides.style.transform = `translateX(-${index * 100}%)`;
+      }
+
+      slider.querySelector('.prev').addEventListener('click', () => {
+          showSlide(--index);
+      });
+
+      slider.querySelector('.next').addEventListener('click', () => {
+          showSlide(++index);
+      });
   });
-  
+});
