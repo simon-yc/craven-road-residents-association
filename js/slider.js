d9709de2305689861d84e71a1573763e4d9bda2f
syc <83619071+simon-yc@users.noreply.github.com>
2024-06-22 17:09:53 -0400

    add description to laneway photos + fix js slider element

--- js/slider.js
+++ js/slider.js
@@ -5,32 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
         const slides = slider.querySelector('.slides');
         const allSlides = slider.querySelectorAll('.photo-item');
         let index = 0;
-  
+
         function showSlide(n) {
             if (n >= allSlides.length) index = 0;
             if (n < 0) index = allSlides.length - 1;
             slides.style.transform = `translateX(-${index * 100}%)`;
             adjustHeight();
         }
-  
+
         function adjustHeight() {
             const currentSlide = allSlides[index];
             const currentImage = currentSlide.querySelector('img');
+            const currentDescription = currentSlide.querySelector('.photo-description');
             const aspectRatio = currentImage.naturalHeight / currentImage.naturalWidth;
-            currentSlide.style.height = `${currentSlide.clientWidth * aspectRatio}px`;
+            const imageHeight = currentSlide.clientWidth * aspectRatio;
+            const descriptionHeight = currentDescription ? currentDescription.offsetHeight : 0;
+            currentSlide.style.height = `${imageHeight + descriptionHeight}px`;
         }
-  
+
         slider.querySelector('.prev').addEventListener('click', () => {
             showSlide(--index);
         });
-  
+
         slider.querySelector('.next').addEventListener('click', () => {
             showSlide(++index);
         });
-  
-        // Initial adjustment
+
         window.addEventListener('resize', adjustHeight);
-        adjustHeight();
+        adjustHeight(); // Initial adjustment
     });
-  });
-  
+});
